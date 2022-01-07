700518e956b58232d29045177bda4a763b55886b
mayun <mayun@126.com>
2022-01-07 10:16:07 +0800

    Initial commit

---
+++ src/main/java/com/huashan/jianfa/Dugu9jian.java
@@ -0,0 +1,12 @@
+package com.huashan.jianfa;
+
+public class Dugu9jian {
+    public static void main(String[] args) {
+        System.out.println("独孤九剑");
+        System.out.println("独孤九剑");
+        System.out.println("独孤九剑");
+        System.out.println("独孤九剑");
+        System.out.println("独孤九剑");
+        System.out.println("独孤九剑");
+    }
+}
